e9e2b233436ee0fb0b821ca550321ab97c325ff9
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-20 15:32:30 -0600

    more

--- w7d1PostIt/server/controllers/AlbumsController.js
+++ w7d1PostIt/server/controllers/AlbumsController.js
@@ -11,7 +11,7 @@ export class AlbumsController extends BaseController {
     super('albums')
     this.router
       .get('', this.getAll)
-    // .get('/:id', this.getById)
+      .get('/:id', this.getById)
     // .use(Auth0Provider.getAuthorizedUserInfo)
     // .post('', this.createAlbum)
     // .put('/:id', this.editAlbum)
@@ -29,21 +29,22 @@ export class AlbumsController extends BaseController {
     }
   }
 
-  // getById(req, res, next) {
-  //   try {
+  async getById(req, res, next) {
+    try {
+      const album = await albumsService.getById(req.params.id)
+      res.send(album)
+    } catch (error) {
+      next(error)
+    }
+  }
 
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
-
-  // createAlbum(req, res, next) {
-  //   try {
-
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
+  async reateAlbum(req, res, next) {
+    try {
+      const album = await albumsService.createAlbum(req.body)
+    } catch (error) {
+      next(error)
+    }
+  }
 
   // editAlbum(req, res, next) {
   //   try {
